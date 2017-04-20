0f5f0d2824641a9ee47020b167423c6761d98aeb
bbrianti <brian.brianti@gmail.com>
2017-04-20 12:58:19 -0400

    Add column component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-class App extends Component {
+let nextTodoId = 1;
+//const App = connect(null, null)(App);
+class Column extends Component {
+  componentDidMount() {
+    const { store } = this.context;
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
 
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const props = this.props;
+    const { store } = this.context;
+    const state = store.getState();
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_ITEM',
+            text: this.input.value,
+            column: props.col,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+        }}>
+          Add
+        </button>
+        <ul>
+          {state.columns[props.col].map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+Column.contextTypes = {
+  store : React.PropTypes.object
+};
+
+
+class App extends Component {
   render() {
     return (
       <div>
-        <div className='column'>Start</div>
-        <div className='column'>Stop</div>
-        <div className='column'>Continue</div>
+        <div className='column'>
+          Start
+          <Column col={0}/>
+        </div>
+        <div className='column'>
+          Stop
+          <Column col={1}/>
+        </div>
+        <div className='column'>
+          Continue
+          <Column col={2}/>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  columns: [
+    [],
+    [],
+    []
+  ]
+}
+
+const reducer = (state = [], action) => {
+  console.log(state);
+  switch (action.type) {
+    case 'ADD_ITEM':
+      var newState = Object.assign({}, state);
+      newState.columns[action.column].push({
+        text: action.text,
+        id: action.id
+      });
+      console.log(newState);
+      return state;
+    default:
+      return state;
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStore(reducer, initialState)}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
